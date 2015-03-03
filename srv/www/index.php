0e48041131885f25365c130ee0a1a63e257f122b
Ganesha <reekoheek@gmail.com>
2015-03-03 14:40:46 +0700

    initial commit

---
+++ srv/www/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Docker Web Arch</title>
+</head>
+<body>
+    <h1>docker-web-arch</h1>
+    <p>Hello!</p>
+    <p>This is your first time to use this archetype. You can change this page from /srv/www directory.</p>
+    <p>To see your PHP information <a href="./info.php">here</a>. To access rockmongo <a href="./rockmongo/">here</a>.</p>
+    <hr>
+    <p>Time: <?php echo date('d/m/Y H:i:s') ?></p>
+</body>
+</html>
